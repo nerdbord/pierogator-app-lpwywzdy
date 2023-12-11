d846d6f13a404fb8ad98083c516142f260e4817d
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-12-11 15:07:25 +0100

    some changes i nheader

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styles from './App.module.css';
-
+import HeaderBackground from './components/Header/HeaderBackground/HeaderBackground';
 // components
 
 import NewPierog from './components/NewPierogConfig/NewPierog';
@@ -7,7 +7,9 @@ import NewPierog from './components/NewPierogConfig/NewPierog';
 const App = () => {
    return (
       <div className={styles.mainWrapper}>
-         <h1>Pierogator świąteczny</h1>
+         <HeaderBackground background={'dumplingMachine'}></HeaderBackground>
+         {/* <HeaderBackground background={'dumplingStore'}></HeaderBackground> */}
+         {/* <h1>Pierogator świąteczny</h1> */}
          <NewPierog />
       </div>
    );
---
+++ src/components/Header/HeaderBackground/HeaderBackground.tsx
@@ -0,0 +1,20 @@
+// styles
+import styles from './HeaderBackground.module.css';
+import SVGComponentMachine from '../SVGComponent/SVGComponentMachine';
+import SVGComponentStore from '../SVGComponent/SVGComponentStore';
+import React from 'react';
+
+interface HeaderBackgroundProps {
+   background: string;
+}
+
+const HeaderBackground: React.FC<HeaderBackgroundProps> = ({ background }) => {
+   return (
+      <header className={`${styles.header}`}>
+         {background === 'dumplingMachine' ? <SVGComponentMachine /> : ''} 
+         {background === 'dumplingStore' ? <SVGComponentStore /> : ''}
+      </header>
+   );
+};
+
+export default HeaderBackground;
