d646a002999383afa21bb5e2382d0ac29c17ae0d
fwas <fwas.corp@gmail.com>
2023-12-11 20:23:54 +0100

    Revert "some changes i nheader"
    This reverts commit d846d6f13a404fb8ad98083c516142f260e4817d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styles from './App.module.css';
-import HeaderBackground from './components/Header/HeaderBackground/HeaderBackground';
+
 // components
 
 import NewPierog from './components/NewPierogConfig/NewPierog';
@@ -7,9 +7,7 @@ import NewPierog from './components/NewPierogConfig/NewPierog';
 const App = () => {
    return (
       <div className={styles.mainWrapper}>
-         <HeaderBackground background={'dumplingMachine'}></HeaderBackground>
-         {/* <HeaderBackground background={'dumplingStore'}></HeaderBackground> */}
-         {/* <h1>Pierogator świąteczny</h1> */}
+         <h1>Pierogator świąteczny</h1>
          <NewPierog />
       </div>
    );
