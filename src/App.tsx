d2542c13e58ee53bdf74f6168a2bfd19026e5ce3
meczajkowski <michal.erik.czajkowski@gmail.com>
2023-12-13 01:43:38 +0100

    feat: add header UI - remove padding from header

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import styles from './App.module.css';
 import HeaderBackground from './components/UI/Header/HeaderBackground/HeaderBackground';
 import NewPierog from './components/NewPierogConfig/NewPierog';
 import { HeaderType } from './enums/enums';
+import { useState } from 'react';
 
-   const App = () => {
+const App = () => {
+   const [newPierogStep, setNewPierogStep] = useState(true);
 
-      return (
-         <div className={styles.mainWrapper}>
-            <HeaderBackground type={HeaderType.machine}></HeaderBackground>
+   return (
+      <div className={styles.mainWrapper}>
+         <HeaderBackground
+            type={newPierogStep ? HeaderType.machine : HeaderType.store}
+         ></HeaderBackground>
+         <div className={styles.appWrapper}>
             <NewPierog />
          </div>
-      );
-   };
+      </div>
+   );
+};
 
-   export default App;
+export default App;
